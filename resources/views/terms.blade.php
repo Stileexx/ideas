3e59cb88f9e456d45fda1227baff88d1407aa8e4
Samir <122364498+Stileexx@users.noreply.github.com>
2024-09-04 17:59:00 +0200

    Blade template

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi iusto maiores omnis beatae unde, dolore perspiciatis
+        vel vitae repudiandae expedita, porro, autem doloribus magnam laboriosam eos molestiae doloremque quas? Cumque quos,
+        temporibus odit dolores, ullam, illum consectetur accusantium quia voluptas pariatur deleniti. Illo, beatae sunt
+        impedit veniam accusamus laudantium recusandae.
+    </div>
+@endsection
